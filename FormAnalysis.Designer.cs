8dd9bfc8927543fc51cbb4077898dfd9e47d6a3c
Xtrendence <xtrendence@yahoo.com>
2019-11-28 00:31:43 +0000

    Added "Previous" and "Next" buttons to the "Analysis" form.
    The "Paint" event listener would get triggered every time the user scrolled, which would cause a lot of lag as the program basically had to render multiple rectangles and perform calculations with each scroll. This obviously led to crashes and a lot of lag.

--- FormAnalysis.Designer.cs
+++ FormAnalysis.Designer.cs
@@ -30,80 +30,125 @@
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAnalysis));
 			this.panelNeighborhood = new System.Windows.Forms.Panel();
-			this.labelNeighborhood = new System.Windows.Forms.Label();
 			this.panelDistrictNames = new System.Windows.Forms.Panel();
 			this.panelProperty = new System.Windows.Forms.Panel();
-			this.labelProperty = new System.Windows.Forms.Label();
 			this.panelNeighborhoodNames = new System.Windows.Forms.Panel();
-			this.panelNeighborhood.SuspendLayout();
-			this.panelProperty.SuspendLayout();
+			this.buttonPreviousDistrict = new System.Windows.Forms.Button();
+			this.buttonNextDistrict = new System.Windows.Forms.Button();
+			this.buttonPreviousNeighborhood = new System.Windows.Forms.Button();
+			this.buttonNextNeighborhood = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// panelNeighborhood
 			// 
-			this.panelNeighborhood.AutoSize = true;
-			this.panelNeighborhood.Controls.Add(this.labelNeighborhood);
 			this.panelNeighborhood.Location = new System.Drawing.Point(12, 12);
 			this.panelNeighborhood.Name = "panelNeighborhood";
-			this.panelNeighborhood.Size = new System.Drawing.Size(1424, 371);
+			this.panelNeighborhood.Size = new System.Drawing.Size(1414, 331);
 			this.panelNeighborhood.TabIndex = 4;
 			this.panelNeighborhood.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelNeighborhood_Paint);
-			// 
-			// labelNeighborhood
-			// 
-			this.labelNeighborhood.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
-			this.labelNeighborhood.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.labelNeighborhood.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-			this.labelNeighborhood.Location = new System.Drawing.Point(20, 10);
-			this.labelNeighborhood.Name = "labelNeighborhood";
-			this.labelNeighborhood.Size = new System.Drawing.Size(0, 1);
-			this.labelNeighborhood.TabIndex = 3;
-			this.labelNeighborhood.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// panelDistrictNames
 			// 
 			this.panelDistrictNames.AutoSize = true;
-			this.panelDistrictNames.Location = new System.Drawing.Point(12, 404);
+			this.panelDistrictNames.Location = new System.Drawing.Point(12, 361);
 			this.panelDistrictNames.Name = "panelDistrictNames";
 			this.panelDistrictNames.Size = new System.Drawing.Size(1414, 50);
 			this.panelDistrictNames.TabIndex = 1;
 			// 
 			// panelProperty
 			// 
-			this.panelProperty.AutoSize = true;
-			this.panelProperty.Controls.Add(this.labelProperty);
-			this.panelProperty.Location = new System.Drawing.Point(12, 460);
+			this.panelProperty.Location = new System.Drawing.Point(12, 457);
 			this.panelProperty.Name = "panelProperty";
-			this.panelProperty.Size = new System.Drawing.Size(1424, 349);
+			this.panelProperty.Size = new System.Drawing.Size(1414, 327);
 			this.panelProperty.TabIndex = 5;
 			this.panelProperty.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelProperty_Paint);
-			// 
-			// labelProperty
-			// 
-			this.labelProperty.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
-			this.labelProperty.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.labelProperty.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-			this.labelProperty.Location = new System.Drawing.Point(20, 10);
-			this.labelProperty.Name = "labelProperty";
-			this.labelProperty.Size = new System.Drawing.Size(0, 1);
-			this.labelProperty.TabIndex = 3;
-			this.labelProperty.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// panelNeighborhoodNames
 			// 
 			this.panelNeighborhoodNames.AutoSize = true;
-			this.panelNeighborhoodNames.Location = new System.Drawing.Point(12, 830);
+			this.panelNeighborhoodNames.Location = new System.Drawing.Point(12, 801);
 			this.panelNeighborhoodNames.Name = "panelNeighborhoodNames";
 			this.panelNeighborhoodNames.Size = new System.Drawing.Size(1414, 52);
 			this.panelNeighborhoodNames.TabIndex = 6;
+			// 
+			// buttonPreviousDistrict
+			// 
+			this.buttonPreviousDistrict.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+			this.buttonPreviousDistrict.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.buttonPreviousDistrict.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+			this.buttonPreviousDistrict.FlatAppearance.BorderSize = 0;
+			this.buttonPreviousDistrict.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonPreviousDistrict.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonPreviousDistrict.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+			this.buttonPreviousDistrict.Location = new System.Drawing.Point(12, 417);
+			this.buttonPreviousDistrict.Name = "buttonPreviousDistrict";
+			this.buttonPreviousDistrict.Size = new System.Drawing.Size(86, 34);
+			this.buttonPreviousDistrict.TabIndex = 19;
+			this.buttonPreviousDistrict.Text = "Previous";
+			this.buttonPreviousDistrict.UseVisualStyleBackColor = false;
+			this.buttonPreviousDistrict.Click += new System.EventHandler(this.ButtonPreviousDistrict_Click);
+			// 
+			// buttonNextDistrict
+			// 
+			this.buttonNextDistrict.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+			this.buttonNextDistrict.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.buttonNextDistrict.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+			this.buttonNextDistrict.FlatAppearance.BorderSize = 0;
+			this.buttonNextDistrict.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonNextDistrict.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonNextDistrict.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+			this.buttonNextDistrict.Location = new System.Drawing.Point(1340, 417);
+			this.buttonNextDistrict.Name = "buttonNextDistrict";
+			this.buttonNextDistrict.Size = new System.Drawing.Size(86, 34);
+			this.buttonNextDistrict.TabIndex = 20;
+			this.buttonNextDistrict.Text = "Next";
+			this.buttonNextDistrict.UseVisualStyleBackColor = false;
+			this.buttonNextDistrict.Click += new System.EventHandler(this.ButtonNextDistrict_Click);
+			// 
+			// buttonPreviousNeighborhood
+			// 
+			this.buttonPreviousNeighborhood.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+			this.buttonPreviousNeighborhood.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.buttonPreviousNeighborhood.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+			this.buttonPreviousNeighborhood.FlatAppearance.BorderSize = 0;
+			this.buttonPreviousNeighborhood.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonPreviousNeighborhood.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonPreviousNeighborhood.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+			this.buttonPreviousNeighborhood.Location = new System.Drawing.Point(12, 859);
+			this.buttonPreviousNeighborhood.Name = "buttonPreviousNeighborhood";
+			this.buttonPreviousNeighborhood.Size = new System.Drawing.Size(86, 34);
+			this.buttonPreviousNeighborhood.TabIndex = 21;
+			this.buttonPreviousNeighborhood.Text = "Previous";
+			this.buttonPreviousNeighborhood.UseVisualStyleBackColor = false;
+			this.buttonPreviousNeighborhood.Click += new System.EventHandler(this.ButtonPreviousNeighborhood_Click);
+			// 
+			// buttonNextNeighborhood
+			// 
+			this.buttonNextNeighborhood.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+			this.buttonNextNeighborhood.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.buttonNextNeighborhood.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+			this.buttonNextNeighborhood.FlatAppearance.BorderSize = 0;
+			this.buttonNextNeighborhood.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonNextNeighborhood.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonNextNeighborhood.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+			this.buttonNextNeighborhood.Location = new System.Drawing.Point(1340, 859);
+			this.buttonNextNeighborhood.Name = "buttonNextNeighborhood";
+			this.buttonNextNeighborhood.Size = new System.Drawing.Size(86, 34);
+			this.buttonNextNeighborhood.TabIndex = 22;
+			this.buttonNextNeighborhood.Text = "Next";
+			this.buttonNextNeighborhood.UseVisualStyleBackColor = false;
+			this.buttonNextNeighborhood.Click += new System.EventHandler(this.ButtonNextNeighborhood_Click);
 			// 
 			// FormAnalysis
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.AutoScroll = true;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
 			this.ClientSize = new System.Drawing.Size(1438, 905);
+			this.Controls.Add(this.buttonNextNeighborhood);
+			this.Controls.Add(this.buttonPreviousNeighborhood);
+			this.Controls.Add(this.buttonNextDistrict);
+			this.Controls.Add(this.buttonPreviousDistrict);
 			this.Controls.Add(this.panelNeighborhoodNames);
 			this.Controls.Add(this.panelProperty);
 			this.Controls.Add(this.panelDistrictNames);
@@ -114,8 +159,6 @@
 			this.MaximumSize = new System.Drawing.Size(1454, 944);
 			this.Name = "FormAnalysis";
 			this.Text = "Analysis";
-			this.panelNeighborhood.ResumeLayout(false);
-			this.panelProperty.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -123,10 +166,12 @@
 
 		#endregion
 		private System.Windows.Forms.Panel panelNeighborhood;
-		private System.Windows.Forms.Label labelNeighborhood;
 		private System.Windows.Forms.Panel panelDistrictNames;
 		private System.Windows.Forms.Panel panelProperty;
-		private System.Windows.Forms.Label labelProperty;
 		private System.Windows.Forms.Panel panelNeighborhoodNames;
+		private System.Windows.Forms.Button buttonPreviousDistrict;
+		private System.Windows.Forms.Button buttonNextDistrict;
+		private System.Windows.Forms.Button buttonPreviousNeighborhood;
+		private System.Windows.Forms.Button buttonNextNeighborhood;
 	}
 }
--- FormAnalysis.cs
+++ FormAnalysis.cs
@@ -13,107 +13,215 @@ namespace SOFT152Assignment
 {
 	public partial class FormAnalysis : Form
 	{
-		public bool resizeNeighborhood = true;
-		public bool resizeProperty = true;
+		public bool drawingEnabled;
+		public int districtStart;
+		public int neighborhoodStart;
+		public int districtEnd;
+		public int neighborhoodEnd;
+		public int neighborhoodTotal;
 		public FormAnalysis()
 		{
 			InitializeComponent();
+			this.districtStart = 0;
+			this.neighborhoodStart = 0;
+			this.districtEnd = 6;
+			this.neighborhoodEnd = 6;
+			this.neighborhoodTotal = 0;
+			this.drawingEnabled = true;
+			panelNeighborhood.Invalidate();
+			panelProperty.Invalidate();
+		}
+  
+		private void DrawBars(int start, int end, string category)
+		{
+			int nameAdjustment = 10;
+			int barWidth = 60;
+			int totalWidth = 120 + barWidth + 22;
+			
+			Font font = new Font("Arial", 12, FontStyle.Bold, GraphicsUnit.Point);
+
+			if(category == "neighborhood")
+			{
+				int panelNeighborhoodHeight = panelNeighborhood.Height;
+				int neighborhoodBarHeightAdjustment = 5;
+				int marginNeighborhood = 58;
+				int marginDistrictName = 0;
+
+				panelDistrictNames.Controls.Clear();
+
+				SolidBrush blueBrush = new SolidBrush(Color.FromArgb(0, 125, 255));
+				for(int i = start; i < end; i++)
+				{
+					try
+					{
+						District district = Data.districts[i];
+						int neighborhoodCount = district.NeighborhoodCount;
+
+						Graphics neighborhoodBar = panelNeighborhood.CreateGraphics();
+
+						int neighborhoodBarHeight = neighborhoodCount * neighborhoodBarHeightAdjustment;
+						int neighborhoodBarTop = panelNeighborhoodHeight - neighborhoodBarHeight;
+
+						neighborhoodBar.FillRectangle(blueBrush, new Rectangle(marginNeighborhood, neighborhoodBarTop, barWidth, neighborhoodBarHeight));
+						neighborhoodBar.Dispose();
+						marginNeighborhood += totalWidth;
+
+						Label labelDistrictName = new Label();
+						labelDistrictName.Text = district.Name + " (" + neighborhoodCount + ")";
+						labelDistrictName.AutoSize = false;
+						labelDistrictName.ForeColor = Color.FromArgb(245, 245, 245);
+						labelDistrictName.Font = font;
+						labelDistrictName.Width = totalWidth - nameAdjustment;
+						labelDistrictName.TextAlign = ContentAlignment.MiddleCenter;
+						labelDistrictName.Top = 0;
+						labelDistrictName.Left = marginDistrictName;
+						panelDistrictNames.Controls.Add(labelDistrictName);
+						marginDistrictName += totalWidth;
+					}
+					catch(IndexOutOfRangeException e)
+					{
+						Debug.WriteLine(e.Message);
+					}
+				}
+				blueBrush.Dispose();
+			}
+			else if(category == "property")
+			{
+				int panelPropertyHeight = panelProperty.Height;
+				double propertyBarHeightAdjustment = 1.5;
+				int marginProperty = 58;
+				int marginNeighborhoodName = 0;
+
+				panelNeighborhoodNames.Controls.Clear();
+
+				SolidBrush orangeBrush = new SolidBrush(Color.FromArgb(230, 150, 0));
+				foreach(District district in Data.districts)
+				{
+					for(int i = start; i < end; i++)
+					{
+						try
+						{
+							Neighborhood neighborhood = district.Neighborhoods[i];
+							int propertyCount = neighborhood.PropertyCount;
+
+							Graphics propertyBar = panelProperty.CreateGraphics();
+
+							int propertyBarHeight = Convert.ToInt32(Math.Round(propertyCount * propertyBarHeightAdjustment));
+							int propertyBarTop = panelPropertyHeight - propertyBarHeight;
+
+							Rectangle rectangle = new Rectangle(marginProperty, propertyBarTop, barWidth, propertyBarHeight);
+							propertyBar.FillRectangle(orangeBrush, rectangle);
+							propertyBar.Dispose();
+							marginProperty += totalWidth;
+
+							Label labelNeighborhoodName = new Label();
+							labelNeighborhoodName.Text = neighborhood.Name + " (" + propertyCount + ")";
+							labelNeighborhoodName.AutoSize = false;
+							labelNeighborhoodName.ForeColor = Color.FromArgb(245, 245, 245);
+							labelNeighborhoodName.Font = font;
+							labelNeighborhoodName.Width = totalWidth - nameAdjustment;
+							labelNeighborhoodName.TextAlign = ContentAlignment.MiddleCenter;
+							labelNeighborhoodName.Top = 0;
+							labelNeighborhoodName.Left = marginNeighborhoodName;
+							panelNeighborhoodNames.Controls.Add(labelNeighborhoodName);
+							marginNeighborhoodName += totalWidth;
+						}
+						catch(IndexOutOfRangeException e)
+						{
+							Debug.WriteLine(e.Message);
+						}
+					}
+				}
+				orangeBrush.Dispose();
+			}
 		}
 
+		private void ButtonPreviousDistrict_Click(object sender, EventArgs e)
+		{
+			if(this.districtStart - 6 > Data.districts.Length)
+			{
+				this.districtStart -= 6;
+			}
+			if(this.districtEnd - 6 > Data.districts.Length)
+			{
+				this.districtEnd -= 6;
+			}
+			panelNeighborhood.Invalidate();
+		}
+
+		private void ButtonNextDistrict_Click(object sender, EventArgs e)
+		{
+			if(this.districtStart + 6 < Data.districts.Length)
+			{
+				this.districtStart += 6;
+			}
+			if(this.districtEnd + 6 < Data.districts.Length)
+			{
+				this.districtEnd += 6;
+			}
+			panelNeighborhood.Invalidate();
+		}
+
+		private void ButtonPreviousNeighborhood_Click(object sender, EventArgs e)
+		{
+			if(this.neighborhoodStart - 6 < this.neighborhoodTotal)
+			{
+				this.neighborhoodStart -= 6;
+			}
+			if(this.neighborhoodEnd - 6 < this.neighborhoodTotal)
+			{
+				this.neighborhoodEnd -= 6;
+			}
+			panelProperty.Invalidate();
+		}
+
+		private void ButtonNextNeighborhood_Click(object sender, EventArgs e)
+		{
+			if(this.neighborhoodStart + 6 < this.neighborhoodTotal)
+			{
+				this.neighborhoodStart += 6;
+			}
+			if(this.neighborhoodEnd + 6 < this.neighborhoodTotal)
+			{
+				this.neighborhoodEnd += 6;
+			}
+			panelProperty.Invalidate();
+		}
+  
 		private void PanelNeighborhood_Paint(object sender, PaintEventArgs e)
 		{
-			Analyze();
+			if(this.drawingEnabled)
+			{
+				if(this.districtStart < 0)
+				{
+					this.districtStart = 0;
+				}
+				if(this.districtEnd > Data.districts.Length)
+				{
+					this.districtEnd = Data.districts.Length;
+				}
+				DrawBars(this.districtStart, this.districtEnd, "neighborhood");
+			}
 		}
 
 		private void PanelProperty_Paint(object sender, PaintEventArgs e)
 		{
-			Analyze();
-		}
-  
-		private void Analyze()
-		{
-			int panelNeighborhoodHeight = panelNeighborhood.Height;
-			int panelPropertyHeight = panelProperty.Height;
-
-			int neighborhoodBarHeightAdjustment = 5;
-			double propertyBarHeightAdjustment = 1.5;
-			int nameAdjustment = 0;
-			int barWidth = 60;
-			int totalWidth = 120 + barWidth;
-			int marginNeighborhood = 58;
-			int marginProperty = 58;
-			int marginDistrictName = 0;
-			int marginNeighborhoodName = 0;
-
-			SolidBrush blueBrush = new SolidBrush(Color.FromArgb(0, 125, 255));
-			SolidBrush orangeBrush = new SolidBrush(Color.FromArgb(230, 150, 0));
-			Font font = new Font("Arial", 12, FontStyle.Bold, GraphicsUnit.Point);
-
-			foreach(District district in Data.districts)
+			if(this.drawingEnabled)
 			{
-				int neighborhoodCount = district.NeighborhoodCount;
-				
-				if(this.resizeNeighborhood)
+				foreach(District district in Data.districts)
 				{
-					labelNeighborhood.Width += totalWidth;
+					this.neighborhoodTotal += district.NeighborhoodCount;
 				}
-
-				Graphics neighborhoodBar = panelNeighborhood.CreateGraphics();
-
-				int neighborhoodBarHeight = neighborhoodCount * neighborhoodBarHeightAdjustment;
-				int neighborhoodBarTop = panelNeighborhoodHeight - neighborhoodBarHeight;
-
-				neighborhoodBar.FillRectangle(blueBrush, new Rectangle(marginNeighborhood, neighborhoodBarTop, barWidth, neighborhoodBarHeight));
-				neighborhoodBar.Dispose();
-				marginNeighborhood += totalWidth;
-
-				Label labelDistrictName = new Label();
-				labelDistrictName.Text = district.Name + " (" + neighborhoodCount + ")";
-				labelDistrictName.AutoSize = false;
-				labelDistrictName.ForeColor = Color.FromArgb(245, 245, 245);
-				labelDistrictName.Font = font;
-				labelDistrictName.Width = totalWidth - nameAdjustment;
-				labelDistrictName.TextAlign = ContentAlignment.MiddleCenter;
-				labelDistrictName.Top = 0;
-				labelDistrictName.Left = marginDistrictName;
-				panelDistrictNames.Controls.Add(labelDistrictName);
-				marginDistrictName += totalWidth;
-
-				foreach(Neighborhood neighborhood in district.Neighborhoods)
+				if(this.neighborhoodStart < 0)
 				{
-					int propertyCount = neighborhood.PropertyCount;
-
-					if(this.resizeProperty)
-					{
-						labelProperty.Width += totalWidth;
-					}
-
-					Graphics propertyBar = panelProperty.CreateGraphics();
-
-					int propertyBarHeight = Convert.ToInt32(Math.Round(propertyCount * propertyBarHeightAdjustment));
-					int propertyBarTop = panelPropertyHeight - propertyBarHeight;
-
-					Rectangle rectangle = new Rectangle(marginProperty, propertyBarTop, barWidth, propertyBarHeight);
-					propertyBar.FillRectangle(orangeBrush, rectangle);
-					propertyBar.Dispose();
-					marginProperty += totalWidth;
-
-					Label labelNeighborhoodName = new Label();
-					labelNeighborhoodName.Text = neighborhood.Name + " (" + propertyCount + ")";
-					labelNeighborhoodName.AutoSize = false;
-					labelNeighborhoodName.ForeColor = Color.FromArgb(245, 245, 245);
-					labelNeighborhoodName.Font = font;
-					labelNeighborhoodName.Width = totalWidth - nameAdjustment;
-					labelNeighborhoodName.TextAlign = ContentAlignment.MiddleCenter;
-					labelNeighborhoodName.Top = 0;
-					labelNeighborhoodName.Left = marginNeighborhoodName;
-					panelNeighborhoodNames.Controls.Add(labelNeighborhoodName);
-					marginNeighborhoodName += totalWidth;
+					this.neighborhoodStart = 0;
 				}
+				if(this.neighborhoodEnd > this.neighborhoodTotal - 1)
+				{
+					this.neighborhoodEnd = this.neighborhoodTotal - 1;
+				}
+				DrawBars(this.neighborhoodStart, this.neighborhoodEnd, "property");
 			}
-			this.resizeNeighborhood = false;
-			this.resizeProperty = false;
-			blueBrush.Dispose();
 		}
 	}
 }
