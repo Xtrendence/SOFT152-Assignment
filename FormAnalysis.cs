a67d5090fbb36e74781ffdc156bb1efcad7a1720
Xtrendence <xtrendence@yahoo.com>
2019-11-28 01:57:40 +0000

    "Analysis" form now works as intended.

--- FormAnalysis.cs
+++ FormAnalysis.cs
@@ -14,20 +14,41 @@ namespace SOFT152Assignment
 	public partial class FormAnalysis : Form
 	{
 		public bool drawingEnabled;
+		public int barsVisible;
+		public int barsTrack;
+
 		public int districtStart;
 		public int neighborhoodStart;
+
 		public int districtEnd;
 		public int neighborhoodEnd;
-		public int neighborhoodTotal;
+
 		public Neighborhood[] neighborhoods = new Neighborhood[0];
+
 		public FormAnalysis()
 		{
 			InitializeComponent();
 			this.districtStart = 0;
 			this.neighborhoodStart = 0;
-			this.districtEnd = 7;
-			this.neighborhoodEnd = 7;
+			this.barsVisible = 7;
+			this.barsTrack = 7;
+			this.districtEnd = barsVisible;
+			this.neighborhoodEnd = barsVisible;
 			this.drawingEnabled = true;
+
+			if(this.neighborhoods.Length == 0)
+			{
+				foreach(District district in Data.districts)
+				{
+					foreach(Neighborhood neighborhood in district.Neighborhoods)
+					{
+						int length = this.neighborhoods.Length;
+						Array.Resize(ref this.neighborhoods, length + 1);
+						this.neighborhoods[length] = neighborhood;
+					}
+				}
+			}
+
 			panelNeighborhood.Invalidate();
 			panelProperty.Invalidate();
 		}
@@ -38,7 +59,7 @@ namespace SOFT152Assignment
 			int barWidth = 60;
 			int totalWidth = 120 + barWidth + 22;
 			
-			Font font = new Font("Arial", 12, FontStyle.Bold, GraphicsUnit.Point);
+			Font font = new Font("Arial", 11, FontStyle.Bold, GraphicsUnit.Point);
 
 			if(category == "neighborhood")
 			{
@@ -60,6 +81,10 @@ namespace SOFT152Assignment
 						Graphics neighborhoodBar = panelNeighborhood.CreateGraphics();
 
 						int neighborhoodBarHeight = neighborhoodCount * neighborhoodBarHeightAdjustment;
+						if(neighborhoodBarHeight >= panelNeighborhoodHeight)
+						{
+							neighborhoodBarHeight = panelNeighborhoodHeight - 20;
+						}
 						int neighborhoodBarTop = panelNeighborhoodHeight - neighborhoodBarHeight;
 
 						neighborhoodBar.FillRectangle(blueBrush, new Rectangle(marginNeighborhood, neighborhoodBarTop, barWidth, neighborhoodBarHeight));
@@ -105,6 +130,10 @@ namespace SOFT152Assignment
 						Graphics propertyBar = panelProperty.CreateGraphics();
 
 						int propertyBarHeight = Convert.ToInt32(Math.Round(propertyCount * propertyBarHeightAdjustment));
+						if(propertyBarHeight >= panelPropertyHeight)
+						{
+							propertyBarHeight = panelPropertyHeight - 20;
+						}
 						int propertyBarTop = panelPropertyHeight - propertyBarHeight;
 
 						Rectangle rectangle = new Rectangle(marginProperty, propertyBarTop, barWidth, propertyBarHeight);
@@ -135,25 +164,97 @@ namespace SOFT152Assignment
 
 		private void ButtonPreviousDistrict_Click(object sender, EventArgs e)
 		{
-			
+			if(this.districtStart - barsTrack < 0)
+			{
+				buttonPreviousDistrict.Visible = false;
+				this.districtStart = 0;
+				this.districtEnd = barsTrack;
+			}
+			else
+			{
+				buttonPreviousDistrict.Visible = true;
+				this.districtStart -= barsTrack;
+				this.districtEnd -= barsTrack;
+			}
+			if(this.districtStart + barsTrack < Data.districts.Length)
+			{
+				buttonNextDistrict.Visible = true;
+			}
+			else
+			{
+				buttonNextDistrict.Visible = false;
+			}
+			if(this.districtEnd - barsTrack <= 0)
+			{
+				buttonPreviousDistrict.Visible = false;
+			}
 			panelNeighborhood.Invalidate();
 		}
 
 		private void ButtonNextDistrict_Click(object sender, EventArgs e)
 		{
-			
+			buttonPreviousDistrict.Visible = true;
+			if(this.districtStart + barsTrack >= Data.districts.Length)
+			{
+				buttonNextDistrict.Visible = false;
+				this.districtStart = Data.districts.Length;
+				this.districtEnd = Data.districts.Length;
+			}
+			else
+			{
+				buttonNextDistrict.Visible = true;
+				this.districtStart += barsTrack;
+				this.districtEnd += barsTrack;
+			}
 			panelNeighborhood.Invalidate();
 		}
 
 		private void ButtonPreviousNeighborhood_Click(object sender, EventArgs e)
 		{
-			
+			if(this.neighborhoodStart - barsTrack < 0)
+			{
+				buttonPreviousNeighborhood.Visible = false;
+				this.neighborhoodStart = 0;
+				this.neighborhoodEnd = barsTrack;
+			}
+			else
+			{
+				buttonPreviousNeighborhood.Visible = true;
+				this.neighborhoodStart -= barsTrack;
+				this.neighborhoodEnd -= barsTrack;
+			}
+			if(this.neighborhoodStart + barsTrack < neighborhoods.Length)
+			{
+				buttonNextNeighborhood.Visible = true;
+			}
+			else
+			{
+				buttonNextNeighborhood.Visible = false;
+			}
+			if(this.neighborhoodEnd - barsTrack <= 0)
+			{
+				buttonPreviousNeighborhood.Visible = false;
+			}
 			panelProperty.Invalidate();
 		}
 
 		private void ButtonNextNeighborhood_Click(object sender, EventArgs e)
 		{
-			
+			buttonPreviousNeighborhood.Visible = true;
+			Debug.WriteLine(neighborhoodStart);
+			Debug.WriteLine(neighborhoods.Length);
+			if(this.neighborhoodStart + barsTrack >= neighborhoods.Length)
+			{
+				buttonNextNeighborhood.Visible = false;
+				this.neighborhoodStart = neighborhoods.Length;
+				this.neighborhoodEnd = neighborhoods.Length;
+			}
+			else
+			{
+				buttonNextNeighborhood.Visible = true;
+				this.neighborhoodStart += barsTrack;
+				this.neighborhoodEnd += barsTrack;
+			}
 			panelProperty.Invalidate();
 		}
   
@@ -161,7 +262,10 @@ namespace SOFT152Assignment
 		{
 			if(this.drawingEnabled)
 			{
-				
+				if(this.districtStart + barsTrack >= Data.districts.Length)
+				{
+					buttonNextDistrict.Visible = false;
+				}
 				DrawBars(this.districtStart, this.districtEnd, "neighborhood");
 			}
 		}
@@ -170,19 +274,10 @@ namespace SOFT152Assignment
 		{
 			if(this.drawingEnabled)
 			{
-				if(this.neighborhoods.Length == 0)
+				if(this.neighborhoodStart + barsTrack >= neighborhoods.Length)
 				{
-					foreach(District district in Data.districts)
-					{
-						foreach(Neighborhood neighborhood in district.Neighborhoods)
-						{
-							int length = neighborhoods.Length;
-							Array.Resize(ref this.neighborhoods, length + 1);
-							neighborhoods[length] = neighborhood;
-						}
-					}
+					buttonNextNeighborhood.Visible = false;
 				}
-				
 				DrawBars(this.neighborhoodStart, this.neighborhoodEnd, "property");
 			}
 		}
